e7eb1e05046354ebf6327a53f6a41df60c7a8357
PaulaRo <rostpaul@kkg-do.eu>
2024-09-23 08:32:16 +0200

    Initialer Commit für Repository-Start

---
+++ src/MainControl.java
@@ -0,0 +1,10 @@
+public class MainControl {
+
+    public static void main(String[] args) {
+        new Person("Gustaf",22);
+        new Person("Claudia",31);
+        new Person("Berta",77);
+
+    }
+
+}
---
+++ src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+
+    // Attribute
+    private String name;
+    private int age;
+
+    // Referenzen
+
+    // Konstruktor
+    public Person(String name, int zahl){
+        this.name = name;
+        age = zahl;
+        halloSagen();
+    }
+
+    // Methoden
+    public void halloSagen (){
+        System.out.println("Hi, mein Name ist "+name+". Ich bin "+age+" Jahre alt.");
+    }
+
+}
