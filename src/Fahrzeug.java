ad05a109a4bcb12b3430e7ead51ab5ebe26dc4a1
PaulaRo <rostpaul@kkg-do.eu>
2024-09-25 13:13:18 +0200

    Initialer Commit für Repository-Start

--- src/Fahrzeug.java
+++ src/Fahrzeug.java
@@ -1,2 +1,20 @@
 public class Fahrzeug {
+
+    //Attribute
+    private String marke;
+    private int kilometers;
+
+    //Referenzen
+
+    //Konstruktor
+    public Fahrzeug(String Mercedes, int 1000) {
+        this.marke = Mercedes;
+        this.kilometers = 1000;
+    }
+
+    //Methoden
+    public String autoInfos() {
+
+    }
+
 }
